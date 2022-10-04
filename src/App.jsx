d1fa54adfba3584b125ec00ae9a093df5471708c
matias <pagosptcmatias@gmail.com>
2022-10-04 17:40:47 -0300

    agregamos un spinner como un un componente convencional

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,46 @@ import imagenLogo from "./img/image.png";
 import Frase from "./components/Frase";
 import { useEffect, useState } from "react";
 import "./Estilo.css";
-
+import Spiner from "./components/Spiner";
 
 function App() {
   // aqui va la logica
-  
-  const [personaje, setPersonaje] = useState({})
 
+  const [personaje, setPersonaje] = useState({});
+  const [mostrarSpiner, setMostrarSpiner] = useState(true);
 
   useEffect(() => {
     // siempre pedir api en montaje cuando se la solicite, no en montaje y actualizacion
     consultarApi();
   }, []);
 
-  const consultarApi = async() => {
-  //  try cach es para poner codigo en est mas controlada por si hay problemas
-    try{
-      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
-      console.log(respuesta)
+  const consultarApi = async () => {
+    //  try cach es para poner codigo en est mas controlada por si hay problemas
+    try {
+      setMostrarSpiner(true);
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      console.log(respuesta);
       // json en minuscula es un metodo
-      const dato = await respuesta.json()
-      console.log(dato[0])
+      const dato = await respuesta.json();
+      console.log(dato[0]);
       // aqui se modifica el useState
-      setPersonaje(dato[0])
-    }catch(error){
-      console.log(error)
-
+      setPersonaje(dato[0]);
+      // mostrar el c omponente frase
+      setMostrarSpiner(false);
+    } catch (error) {
+      console.log(error);
     }
   };
+  // condicion logica if, operador ternario ? :
+  // (condicion logica)?(logica a ejecutar si cumplo la condicion logica):(logica a ejecutar si no cumplo la condicion logica)
+  const mostrarComponente =
+    mostrarSpiner === true ? (
+      <Spiner></Spiner>
+    ) : (
+      <Frase personaje={personaje}></Frase>
+    );
 
   return (
     <Card
@@ -43,12 +55,11 @@ function App() {
       </div>
       <div>
         <Button className="my-5" variant="warning" onClick={consultarApi}>
-          
           Obtener Frase
         </Button>
       </div>
 
-      <Frase personaje={personaje}></Frase>
+      {mostrarComponente}
     </Card>
   );
 }
---
+++ src/components/Spiner.jsx
@@ -0,0 +1,18 @@
+import './Spiner.css'
+
+const Spiner = () => {
+    return (
+        <section className='d-flex justify-content-center'>
+        <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
+      </section>
+    );
+};
+
+export default Spiner;
